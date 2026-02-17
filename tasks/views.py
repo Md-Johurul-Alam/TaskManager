82d05346c41af63647dc5d0528f3d17abe9631ad
Md. Johurul Alam <johurul@erainfotechbd.com>
2026-02-17 18:22:57 +0600

    2nd

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Yo!! Wassup!!!")
+    return render(request, 'tasks/home.html')
 
 
 
